70a34e52ca17804ff46449eb508ae5e22a04fbbd
Adi Munawar <munawar.adi31@gmail.com>
2022-01-27 14:46:03 +0700

    menambahkan beberapa library pendukung

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
 import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
+dotenv.config();
 
 class App {
     public app: Application;
 
     constructor() {
         this.app = express();
+        this.plugins();
         this.routes();
+    }
+
+    protected plugins(): void {
+        /* Get Body Request JSON */
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+
+        // Logger dengan morgan
+        this.app.use(morgan('dev'));
+
+        // Beberapa Library Pendukung
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
     }
 
     protected routes(): void {
         this.app.route('/').get((req: Request, res: Response) => {
             res.send("From TS routes");
         })
+
+        this.app.route('/users').post((req: Request, res: Response) => {
+            res.send(req.body);
+        })
     }
 }
 
-const PORT: number = 5000;
-
 const app = new App().app;
-app.listen(PORT, () => console.log(`🔥Server Running in http://localhost:${PORT}`));
+
+/* Get Body Request JSON */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(process.env.PORT, () => console.log(`🔥Server is Running in http://localhost:${process.env.PORT}`));
