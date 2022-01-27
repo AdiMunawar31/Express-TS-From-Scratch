e5c70ab7596143e0d03f50eef7d7dc7688682d13
Adi Munawar <munawar.adi31@gmail.com>
2022-01-27 14:00:56 +0700

    Membuat server sederhana dengan typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello");
+import express, { Application, Request, Response } from 'express';
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.routes();
+    }
+
+    protected routes(): void {
+        this.app.route('/').get((req: Request, res: Response) => {
+            res.send("From TS routes");
+        })
+    }
+}
+
+const PORT: number = 5000;
+
+const app = new App().app;
+app.listen(PORT, () => console.log(`🔥Server Running in http://localhost:${PORT}`));
